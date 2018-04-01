49cce68b61721ff2666ebb9cbcf517de264b2b0d
zachary rock-zelazny <zacharyrock@hotmail.com>
2018-03-31 20:17:13 -0700

    ECS Start

---
+++ Assets/Scripts/ECS/ChunkSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+
+public class ChunkSystem : ComponentSystem {
+
+    public struct Data
+    {
+        public int Length;
+        public GameObjectArray GameObject;
+        public ComponentArray<Rigidbody> Rigidbody;
+        //[ReadOnly] public ComponentDataArray<PlayerInput> PlayerInput;
+        //public SubtractiveComponent<Dead> Dead;
+    }
+
+    [Inject] private Data data;
+
+    protected override void OnUpdate()
+    {
+
+    }
+}
